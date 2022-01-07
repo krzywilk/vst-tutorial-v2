2f17ffb3ebb297b31113329cb40590ba03df4174
krzysiek <krzywilk@gmail.com>
2022-01-07 20:18:28 +0100

    init commit

---
+++ Source/PluginProcessor.h
@@ -0,0 +1,141 @@
+/*
+  ==============================================================================
+
+    This file contains the basic framework code for a JUCE plugin processor.
+
+  ==============================================================================
+*/
+
+#pragma once
+
+#include <JuceHeader.h>
+
+enum Slope
+{
+    Slope_12,
+    Slope_24,
+    Slope_36,
+    Slope_48
+};
+
+struct ChainSettings
+{
+    float peakFreq{ 0 }, peakGainInDecibels{ 0 }, peakQuality{ 1.f };
+    float lowCutFreq{ 0 }, highCutFreq{ 0 };
+    //int lowCutSlope{ 0 }, highCutSlope{ 0 };
+
+    Slope lowCutSlope{ Slope::Slope_12 }, highCutSlope{ Slope::Slope_12 };
+
+    bool lowCutBypassed{ false }, peakBypassed{ false }, highCutBypassed{ false };
+};
+
+using Filter = juce::dsp::IIR::Filter<float>;
+
+using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+
+using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+
+
+enum ChainPositions
+{
+    LowCut,
+    Peak,
+    HighCut
+};
+
+//==============================================================================
+/**
+*/
+class Vsttutorialv2AudioProcessor  : public juce::AudioProcessor
+{
+public:
+    //==============================================================================
+    Vsttutorialv2AudioProcessor();
+    ~Vsttutorialv2AudioProcessor() override;
+
+    //==============================================================================
+    void prepareToPlay (double sampleRate, int samplesPerBlock) override;
+    void releaseResources() override;
+
+   #ifndef JucePlugin_PreferredChannelConfigurations
+    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
+   #endif
+
+    void processBlock (juce::AudioBuffer<float>&, juce::MidiBuffer&) override;
+
+    //==============================================================================
+    juce::AudioProcessorEditor* createEditor() override;
+    bool hasEditor() const override;
+
+    //==============================================================================
+    const juce::String getName() const override;
+
+    bool acceptsMidi() const override;
+    bool producesMidi() const override;
+    bool isMidiEffect() const override;
+    double getTailLengthSeconds() const override;
+
+    //==============================================================================
+    int getNumPrograms() override;
+    int getCurrentProgram() override;
+    void setCurrentProgram (int index) override;
+    const juce::String getProgramName (int index) override;
+    void changeProgramName (int index, const juce::String& newName) override;
+
+    //==============================================================================
+    void getStateInformation (juce::MemoryBlock& destData) override;
+    void setStateInformation (const void* data, int sizeInBytes) override;
+    static juce::AudioProcessorValueTreeState::ParameterLayout createParaneterLayout();
+       
+
+    juce::AudioProcessorValueTreeState apvts{ *this,nullptr,"Params",  createParaneterLayout() }; 
+
+private:
+
+
+    MonoChain leftChain, rightChain;
+
+    void updatePeakFilter(const ChainSettings& chainSettings);
+    using Coefficients = Filter::CoefficientsPtr;
+    static void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+
+    template<typename ChainType, typename CoefficientType>
+    void updateCutFilter(ChainType& cutFilter, const CoefficientType& cutCoefficients,
+        const Slope& cutSlope);
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (Vsttutorialv2AudioProcessor)
+};
+
+template<typename ChainType, typename CoefficientType>
+inline void Vsttutorialv2AudioProcessor::updateCutFilter(ChainType& cutFilter, const CoefficientType& cutCoefficients, const Slope& cutSlope)
+{
+    cutFilter.template setBypassed<0>(true);
+    cutFilter.template setBypassed<1>(true);
+    cutFilter.template setBypassed<2>(true);
+    cutFilter.template setBypassed<3>(true);
+
+    switch (cutSlope)
+    {
+        case Slope_48:
+        {
+            *cutFilter.get<3>().coefficients = *cutCoefficients[0];
+            cutFilter.setBypassed<3>(false);
+        }
+
+        case Slope_36:
+        {
+            *cutFilter.get<2>().coefficients = *cutCoefficients[0];
+            cutFilter.setBypassed<2>(false);
+        }
+        case Slope_24:
+        {
+            *cutFilter.get<1>().coefficients = *cutCoefficients[0];
+            cutFilter.setBypassed<1>(false);
+        }
+        case Slope_12:
+        {
+            *cutFilter.get<0>().coefficients = *cutCoefficients[0];
+            cutFilter.setBypassed<0>(false);
+        }
+    }
+}
